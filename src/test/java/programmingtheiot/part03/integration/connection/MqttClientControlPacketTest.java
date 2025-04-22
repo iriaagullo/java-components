fed78a768368acf7fa7cb8115c3660024eeaa252
iriaagullo <iriaagullo22@gmail.com>
2025-04-22 20:15:36 +0200

    cambiosj7

--- src/test/java/programmingtheiot/part03/integration/connection/MqttClientControlPacketTest.java
+++ src/test/java/programmingtheiot/part03/integration/connection/MqttClientControlPacketTest.java
@@ -9,20 +9,16 @@
 
 package programmingtheiot.part03.integration.connection;
 
-import static org.junit.Assert.*;
-
 import java.util.logging.Logger;
 
 import org.junit.After;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 import org.junit.Before;
 import org.junit.Test;
 
-import programmingtheiot.common.ConfigConst;
-import programmingtheiot.common.ConfigUtil;
-import programmingtheiot.common.IDataMessageListener;
 import programmingtheiot.common.ResourceNameEnum;
-import programmingtheiot.data.*;
-import programmingtheiot.gda.connection.*;
+import programmingtheiot.gda.connection.MqttClientConnector;
 
 /**
  * This test case class contains very basic integration tests for
@@ -64,12 +60,36 @@ public class MqttClientControlPacketTest
 	public void testConnectAndDisconnect()
 	{
 		// TODO: implement this test
+		try {
+			boolean connected = this.mqttClient.connectClient();
+	
+			assertTrue("MQTT client should connect successfully.", connected);
+	
+			Thread.sleep(2000); // espera corta para asegurarse de que CONNECT/CONNACK se completen
+	
+			boolean disconnected = this.mqttClient.disconnectClient();
+	
+			assertTrue("MQTT client should disconnect successfully.", disconnected);
+		} catch (Exception e) {
+			fail("Exception during connect/disconnect: " + e.getMessage());
+		}
 	}
 	
 	@Test
 	public void testServerPing()
 	{
 		// TODO: implement this test
+		try {
+			boolean connected = this.mqttClient.connectClient();
+			assertTrue(connected);
+	
+			// Forzar ping es indirecto: solo esperar sin enviar nada por un momento.
+			Thread.sleep(65000); // Mayor al keep-alive para forzar el envío de PINGREQ
+	
+			this.mqttClient.disconnectClient();
+		} catch (Exception e) {
+			fail("Exception during ping test: " + e.getMessage());
+		}
 	}
 	
 	@Test
@@ -78,6 +98,28 @@ public class MqttClientControlPacketTest
 		// TODO: implement this test
 		// 
 		// IMPORTANT: be sure to use QoS 1 and 2 to see ALL control packets
+		try {
+			boolean connected = this.mqttClient.connectClient();
+			assertTrue(connected);
+	
+			String testPayload = "Control packet test payload";
+			int qos = 2;
+			this.mqttClient.subscribeToTopic(ResourceNameEnum.CDA_SENSOR_MSG_RESOURCE, qos);
+
+			Thread.sleep(1000);
+			
+			this.mqttClient.publishMessage(ResourceNameEnum.CDA_SENSOR_MSG_RESOURCE, testPayload, qos);
+
+			Thread.sleep(2000);
+
+			this.mqttClient.unsubscribeFromTopic(ResourceNameEnum.CDA_SENSOR_MSG_RESOURCE);
+	
+			this.mqttClient.disconnectClient();
+			
+		} catch (Exception e) {
+			fail("Exception during pub/sub test: " + e.getMessage());
+		}
+	
 	}
 	
 }
